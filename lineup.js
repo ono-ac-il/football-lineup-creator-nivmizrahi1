6f8ab8b12c3f8c993962ac592c23dd590aeb514b
nivmizrahi1 <nivmizrahi2000@gmail.com>
2023-03-20 18:36:38 +0200

    goalkeepers

--- lineup.js
+++ lineup.js
@@ -10,5 +10,24 @@ const players = [
     { name: 'Varane', position: 'defender' },
     { name: 'Neuer', position: 'goalkeeper' },
     { name: 'Buffon', position: 'goalkeeper' },
-    { name: 'Casillas', position: 'goalkeeper' }
+    { name: 'Casillas', position: 'goalkeeper' },
+    { name: 'Davidovich', position: 'goalkeeper' }
 ]
+/* filter all the golakeepers from players array */
+
+
+const goalkeepers = players.filter(function (player) {
+    return player.position == 'goalkeeper';
+});
+
+/*var gkStr = '';
+for (let i = 0; i < goalkeepers.length; i++) {
+    // gkStr += '<option value="' + goalkeepers[0].name + '">' + goalkeepers[0].name + '</option >';
+    gkStr += `<option value="${goalkeepers[i].name}">${goalkeepers[i].name}</option>`;
+}*/
+
+const mappedGoalkeepers = goalkeepers.map(function (item) {
+    return `<option value="${item.name}">${item.name}</option>`
+});
+
+document.getElementById('pos1').innerHTML = mappedGoalkeepers.join('');
